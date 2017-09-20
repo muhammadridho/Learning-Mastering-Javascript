cbb85a93552a55faee378947d744e8a46d359689
muhammadridho <ulunmuhammadridho@gmail.com>
2017-09-20 09:11:54 +0700

    adding 1 course

--- es-6-wesbos/generator_function.js
+++ es-6-wesbos/generator_function.js
@@ -9,6 +9,12 @@ function* listPeople(){
 
 const people = listPeople();
 
+function* loop(arr){
+    console.log(inventors);
+    for (const item of arr) 
+	yield item;
+}
+
 const inventors = [
     { first: 'Albert', last: 'Einstein', year: 1879 },
     { first: 'Muhammad', last: 'Ridho', year: 1643 },
@@ -16,3 +22,7 @@ const inventors = [
     { first: 'Nandra', last: 'Juliandra', year: 1998 }
 ]
 console.log(people.next());
+
+const inventorGen = loop(inventors);
+
+console.log(inventorGen.next());
